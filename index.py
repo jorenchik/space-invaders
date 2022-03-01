c1ce844c3c485a8aa17b13a8e4eae0d151c11ef6
jorenchik <jorens.stekels@gmail.com>
2022-03-01 19:18:20 +0200

    Fireball class

--- index.py
+++ index.py
@@ -14,18 +14,26 @@ assets = pathlib.Path(absPath/'assets')
 enemySprites = list(assets.glob("enemy_*.png"))
 
 class Enemy:
-    def __init__(self, sprite=None, x=None, y=None, xChange=None, yChange=None):
+    def __init__(self, sprite=None, pos=None, speed=None):
         spriteIndex = random.randint(0, len(enemySprites)-1)
         sprite = sprite if sprite else enemySprites[spriteIndex]
         self.sprite = pygame.image.load(sprite)
+        if pos:
+            x = pos.x
+            y = pos.y
+        else:
+            x = None
+            y = None
         self.pos = pygame.Vector2(x if x else random.randint(0,736), y if y else random.randint(50,150))
-        speedModule = math.sqrt(pow(.2, 2)+pow(.1,2))
+        if speed:
+            xSpeed = speed.x
+            ySpeed = speed.y
+        else:
+            xSpeed = None
+            ySpeed = None
+        speedModule = math.sqrt(pow(xSpeed if xSpeed else .2, 2)+pow(ySpeed if ySpeed  else .1,2))
         angle = random.uniform(0, 2.0*math.pi)
         self.speed = pygame.Vector2([speedModule* math.cos(angle), speedModule * math.sin(angle)]) 
-        self.x = x if x else random.randint(0,736)
-        self.y = y if y else random.randint(50,150)
-        self.xChange = xChange if xChange else .2
-        self.yChange = yChange if yChange else .1
     def move(self,x,y):
         screen.blit(self.sprite, (x,y))
     def changeDirectionSymmetrically(self, ax):
@@ -56,7 +64,14 @@ class Player:
             return True
         return False
         
-
+class Fireball: 
+    def __init__(self, pos, speed):
+        self.pos = pos
+        self.speed = speed
+        self.sprite = pygame.image.load("assets/fireball_sprite.png")
+        self.state = 'ready'
+    def move(self,x,y):
+        screen.blit(self.sprite, (x,y))
 
 # Settings
 enemyLimit = 7
@@ -68,36 +83,22 @@ gameIcon = pygame.image.load("assets/icon.ico")
 screen = pygame.display.set_mode((800,800))
 screenImage = pygame.image.load("assets/screen.png")
 
-# Helpers
-def isCollision(enemyX, enemyY, fireballX, fireballY):
-    distance = math.sqrt(math.pow(enemyX-fireballX,2)+math.pow(enemyY-fireballY,2))
-    if distance < 40:
-        return True
-    else:
-        return False
 
 # Create entities
 enemies = []
 for i in range(0,enemyLimit):
     enemies.append(Enemy())
 player = Player()
+fireball = Fireball(pygame.Vector2(player.pos.x,player.pos.y), pygame.Vector2(0, -.5))
 
-fireballSprite =  pygame.image.load("assets/fireball_sprite.png")
-fireballX = player.pos.x
-# fireballY has a slight offset
-fireballY = player.pos.x-25
-fireballXChange = 0
-fireballYChange = 0.5
+def isCollision(obj1, obj2, distance):
+    if obj1.pos.distance_to(obj2.pos) <= distance:
+        return True
+    return False
 
-# State variables
-fireballState = "ready"
+# State 
+fireballState = 'ready'
 score = 0
-
-# Display functions
-def fireball(x,y):
-    global fireballState
-    fireballState = "fire"
-    screen.blit(fireballSprite, (x+16,y-10))
 
 # Update cycle
 active = True
@@ -114,10 +115,11 @@ while active:
             if event.key == pygame.K_RIGHT:
                 player.changeSpeedX(.25)
             if event.key == pygame.K_SPACE:
-                if fireballState == "ready":
+                if fireball.state == "ready":
                     # fireballX has a slight offset
-                    fireballX = (player.pos.x-15)
-                    fireball(fireballX,fireballY)
+                    fireball.state = 'fire'
+                    fireball.pos.x = (player.pos.x)
+                    fireball.move(fireball.pos.x,fireball.pos.y)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 player.changeSpeedX(0)
@@ -136,21 +138,23 @@ while active:
             enemy.changeDirectionSymmetrically(borderCollision)
         enemy.move(enemy.pos.x, enemy.pos.y)
 
-
-    # fireball logic
-    # if fireballY <= 0:
-    #     fireballY = playerY
-    #     fireballState = "ready"
-    # if fireballState is "fire":
-    #     fireball(fireballX,fireballY)
-    #     fireballY -= fireballYChange
-    # collision = isCollision(enemyX, enemyY, fireballX, fireballY)
-    # if collision:
-    #     fireballY= playerY
-    #     fireballState ="ready"
-    #     score += 1
-    #     print(score)
-    #     enemyX = random.randint(0,736)
-    #     enemyY = random.randint(50,150)
+    # Fireball logic
+    if fireball.pos.y <= 0:
+        fireball.pos.y = player.pos.y
+        fireball.state = 'ready'
+    if fireball.state == 'fire':
+        fireball.move(fireball.pos.x, fireball.pos.y)
+        fireball.pos.y += fireball.speed.y
+    colidedEnemies = []
+    for enemy in enemies:
+        if isCollision(fireball, enemy, 27):
+            colidedEnemies.append(enemy)
+    if(len(colidedEnemies) > 0):
+        fireball.pos.y = player.pos.y
+        fireball.state = 'ready'
+        score += 1
+        print(score)
+    for enemy in colidedEnemies:
+        enemies.remove(enemy)
             
     pygame.display.update()
