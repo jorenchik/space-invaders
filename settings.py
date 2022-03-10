27add9059aa8590ff7f2b933472a9765fed17531
jorenchik <jorens.stekels@gmail.com>
2022-03-10 06:28:21 +0200

    Settings' comments

--- settings.py
+++ settings.py
@@ -1,13 +1,20 @@
 # Game settings
+
+# General
+fpsLimit = 60
+resolution = (800,800)
 title = "Space invaders"
 icon = "assets/icon.ico"
-resolution = (800,800)
 font_ = "Comic Sans MS"
-fpsLimit = 60
+
+# Gameplay
 enemyLimit = 25
 heartCount = 5
+
+# Sprites' positioning
 startEnemyX = 30
 startEnemyY = 100
+startEnemyY = 50
 endEnemyY = 450
 enemyXGap = 5
 enemyYGap = 5
@@ -17,7 +24,11 @@ leftBorder = 1
 rightBorder = 799
 topBorder = 0
 bottomBorder = 799
+
+# Speed multipliers
 enemySpeed = 2
 fireballSpeed = 8
 playerSpeed = 3
+
+# Hitboxes
 hitboxesVisible = False
